15deb66983e7e4db7a7cb9637cf2351845679245
Michael Lyon <mlyon360@gmail.com>
2024-05-01 16:36:07 -0600

    add debug flag

--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -8,9 +8,18 @@ import (
 )
 
 func Init() {
+	logLevel := slog.LevelInfo
+	debugFlag := os.Getenv("DEBUG")
+
+	if debugFlag == "true" {
+		logLevel = slog.LevelDebug
+	}
+
 	w := os.Stderr
 
 	slog.SetDefault(slog.New(
-		tint.NewHandler(w, &tint.Options{}),
+		tint.NewHandler(w, &tint.Options{
+			Level: logLevel,
+		}),
 	))
 }
