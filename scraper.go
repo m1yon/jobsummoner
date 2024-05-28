097e7edd27186f84ccf1ce4b0bc0e3b1457456a3
Michael Lyon <mlyon360@gmail.com>
2024-05-28 10:32:06 -0600

    add location and worktype to url buider

--- scraper.go
+++ scraper.go
@@ -5,10 +5,20 @@ import (
 	"strings"
 )
 
+type WorkType string
+
+const (
+	WorkTypeOnSite WorkType = "1"
+	WorkTypeRemote WorkType = "2"
+	WorkTypeHybrid WorkType = "3"
+)
+
 const linkedInBaseSearchURL = "https://www.linkedin.com/jobs-guest/jobs/api/seeMoreJobPostings/search"
 
 type ScrapeConfig struct {
 	Keywords []string
+	Location string
+	WorkType []WorkType
 }
 
 func BuildURL(config ScrapeConfig) string {
@@ -16,8 +26,21 @@ func BuildURL(config ScrapeConfig) string {
 
 	q := url.Query()
 	q.Set("keywords", strings.Join(config.Keywords, " OR "))
+	q.Set("location", config.Location)
+	q.Set("f_WT", join(config.WorkType, ","))
 
 	url.RawQuery = q.Encode()
 
 	return url.String()
+}
+
+func join[T ~string](input []T, sep string) string {
+	slice := make([]string, len(input))
+	for i, v := range input {
+		slice[i] = string(v)
+	}
+
+	result := strings.Join(slice, sep)
+
+	return result
 }
--- scraper_test.go
+++ scraper_test.go
@@ -10,9 +10,11 @@ func TestLinkedInPage(t *testing.T) {
 	t.Run("builds correct URL given a config", func(t *testing.T) {
 		got := BuildURL(ScrapeConfig{
 			Keywords: []string{"react", "typescript"},
+			Location: "United States",
+			WorkType: []WorkType{WorkTypeRemote, WorkTypeOnSite},
 		})
 
-		assert.Equal(t, linkedInBaseSearchURL+"?keywords=react+OR+typescript", got)
+		assert.Equal(t, linkedInBaseSearchURL+"?f_WT=2%2C1&keywords=react+OR+typescript&location=United+States", got)
 	})
 	// file := os.File{}
 	// got := CrawlLinkedInPage(file)
